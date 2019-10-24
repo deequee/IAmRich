15e0a63bf2072a9ca70710fbca2e9216d76ba47d
deequee <surdikaybola@gmail.com>
2019-10-24 09:42:42 -0500

    added reset button

--- app/src/main/java/com/example/i_am_rich/MainActivity.kt
+++ app/src/main/java/com/example/i_am_rich/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.i_am_rich
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private var networth = 100.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        plus_btn.setOnClickListener { increaseNetWorth()  }
+        reset_btn.setOnClickListener { resetNetWorth() }
+    }
+
+    private fun increaseNetWorth() {
+        networth *= 10 / 2.5
+        net_value.text = "$$networth"
+    }
+
+    private fun resetNetWorth() {
+        networth = 100.0
+        net_value.text = "$$networth"
+
     }
 }
